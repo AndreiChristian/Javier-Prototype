b8c09d94f9f72f605f164fecad03128c049d1e7f
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-15 19:07:11 +0100

    added validation and visual feedback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'javier-prototype';
+  @ViewChild('form') form: NgForm;
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
+
